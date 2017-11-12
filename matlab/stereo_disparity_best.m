2ccea20ea3e41ec1117a25757d0a75d6f7acfc16
keenan-burnett <keenburn2004@gmail.com>
2017-11-11 20:53:31 -0500

    Working on putting together my dynamic programming solution.

--- matlab/stereo_disparity_best.m
+++ matlab/stereo_disparity_best.m
@@ -33,7 +33,88 @@ function [Id] = stereo_disparity_best(Il, Ir, bbox)
 
 %--- FILL ME IN ---
 
-% Code goes here...
+W = 21;
+max_d = 63;
+
+% Convert to grayscale
+if size(Il,3) > 1
+    gL = rgb2gray(Il);
+end
+if size(Ir,3) > 1
+    gR = rgb2gray(Ir);
+end
+gL = double(gL);
+gR = double(gR);
+
+% Compute squared difference at different disparities
+[m,n] = size(gL);
+Co = zeros(m,n,max_d+1);
+Co(:,:,1) = (gL - gR).^2;
+gR_s = gR;
+for i = 2:max_d+1
+    % Shift
+    gR_s = [zeros(m,1) gR_s];
+    gR_s = gR_s(:,1:n);
+    Ci = (gL - gR_s).^2;
+    % Pad left side
+    L = Ci(:,i);
+    L = repmat(L,1,i-1);
+    Ci(:,1:i-1) = L;
+    % Update Co
+    Co(:,:,i) = Ci;
+end
+
+% Compute SSD using separated horizontal and vertical sum filters
+H = ones(1,W);
+V = ones(W,1);
+C = zeros(m,n,max_d+1);
+for i = 1:max_d+1
+    C(:,:,i) = conv2(Co(:,:,i),H, 'same');
+    C(:,:,i) = conv2(C(:,:,i),V, 'same');
+end
+
+
+y = 1;
+
+lambda = 1;     % Tune: higher = more regularization
+
+Sbest = zeros(max_d+1,n);
+dbest = zeros(max_d+1,n);
+Sbest(:,1) = reshape(C(1,y,:),max_d+1,1); % initialize x = 1
+
+% Loop over each column x starting at col 2
+for x = 2:m
+    % For each disparity value at this column:
+    for d = 1:max_d+1
+        
+        m_x = C(x,y,d);     % Cost at this (x,d)
+        Smin = Inf;
+        dmin = 1;
+        % Find previous x(d) which minimizes cost function
+        for d_1 = 1:max_d+1
+            m_x_1 = C(x-1,y,d_1);
+            smooth_cost = lambda * (d - d_1)^2;
+            S = m_x_1 + smooth_cost;
+            if S < Smin
+                Smin = S;
+                dmin = d_1;
+            end
+        end
+        Sbest(d,x) = Smin + m_x;
+        dbest(d,x) = dmin;
+        
+    end 
+end
+
+
+
+
+
+
+
+
+
+
   
 %------------------
   
